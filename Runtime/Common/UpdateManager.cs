3c778262f9c7dafeb0df7b509566e9a8d2c75c15
cowbear6598 <cowbear6598@gmail.com>
2020-08-13 00:50:43 +0800

    20200813

---
+++ Runtime/Common/UpdateManager.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Soap.Update
+{
+    public class UpdateManager : MonoBehaviour
+    {
+        public static UpdateManager Instance;
+
+        public Action UpdateEvent;
+        public Action FixedUpdateEvent;
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+
+                DontDestroyOnLoad(gameObject);
+            }
+        }
+
+        private void Update()
+        {
+            UpdateEvent?.Invoke();
+        }
+
+        private void FixedUpdate()
+        {
+            FixedUpdateEvent?.Invoke();
+        }
+
+        public void RequestUpdate(Action _action)
+        {
+            UpdateEvent += _action;
+        }
+
+        public void RemoveUpdate(Action _action)
+        {
+            UpdateEvent -= _action;
+        }
+
+        public void RequestFixedUpdate(Action _action)
+        {
+            FixedUpdateEvent += _action;
+        }
+
+        public void RemoveFixedUpdate(Action _action)
+        {
+            FixedUpdateEvent -= _action;
+        }
+    }
+}
--- Runtime/ScriptableObject/MysqlManagerScriptableObject.cs
+++ Runtime/ScriptableObject/MysqlManagerScriptableObject.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MysqlManagerScriptableObject : ScriptableObject
+namespace Soap.Internet
 {
-    public string domainName;
+    public class MysqlManagerScriptableObject : ScriptableObject
+    {
+        public string domainName;
+    }
 }
